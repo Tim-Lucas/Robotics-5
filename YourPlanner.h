c70dc68e6d6991e851492a5b231649ca9ab0011e
Jacenty00 <jacekxp60@gmail.com>
2022-02-15 18:35:30 +0100

    Add Kd-trees for nn-search

--- YourPlanner.h
+++ YourPlanner.h
@@ -2,17 +2,19 @@
 #define _YOUR_PLANNER_H_
 
 #ifndef M_PI
-#define M_PI           3.14159265358979323846
+#define M_PI 3.14159265358979323846
 #endif
 
 #include "RrtConConBase.h"
+#include "YourSampler.h"
+#include <rl/plan/KdtreeNearestNeighbors.h>
 
 using namespace ::rl::plan;
 
 /**
-*	The implementation of your planner.
-*	modify any of the existing methods to improve planning performance.
-*/
+ *	The implementation of your planner.
+ *	modify any of the existing methods to improve planning performance.
+ */
 class YourPlanner : public RrtConConBase
 {
 public:
@@ -24,15 +26,22 @@ public:
 
   bool solve();
 
+  YourSampler *sampler;
+
+  rl::plan::KdtreeNearestNeighbors *nearestNeighbors0;
+  rl::plan::KdtreeNearestNeighbors *nearestNeighbors1;
+
 protected:
-  void choose(::rl::math::Vector& chosen);
-
-  RrtConConBase::Vertex connect(Tree& tree, const Neighbor& nearest, const ::rl::math::Vector& chosen);
-
-  RrtConConBase::Vertex extend(Tree& tree, const Neighbor& nearest, const ::rl::math::Vector& chosen);
+  void choose(::rl::math::Vector &chosen, int index);
 
 private:
+  Neighbor nearest(const Tree &tree, const rl::math::Vector &chosen, int index);
 
+  Vertex connect(Tree &tree, const Neighbor &nearest, const rl::math::Vector &chosen, int index);
+
+  Vertex addVertex(Tree &tree, const VectorPtr &q, int index);
+
+  Vertex addVertex(Tree &tree, const VectorPtr &q);
 };
 
 #endif // _YOUR_PLANNER_H_
--- YourSampler.cpp
+++ YourSampler.cpp
@@ -6,15 +6,34 @@ namespace rl
 {
     namespace plan
     {
-        YourSampler::YourSampler() :
-            Sampler(),
-            randDistribution(0, 1),
-            randEngine(::std::random_device()())
+        YourSampler::YourSampler() : Sampler(),
+                                     randDistribution(0, 1),
+                                     randEngine(::std::random_device()())
         {
         }
 
         YourSampler::~YourSampler()
         {
+        }
+
+        ::rl::math::Vector
+        YourSampler::generate(int index)
+        {
+            ::rl::math::Vector rand(this->model->getDof());
+
+            ::rl::math::Real r = this->rand();
+
+            if (r <= this->goal_prob)
+            {
+                return *this->getGoal(index);
+            }
+
+            for (::std::size_t i = 0; i < this->model->getDof(); ++i)
+            {
+                rand(i) = this->rand();
+            }
+
+            return this->model->generatePositionUniform(rand);
         }
 
         ::rl::math::Vector
@@ -30,17 +49,30 @@ namespace rl
             return this->model->generatePositionUniform(rand);
         }
 
-        ::std::uniform_real_distribution< ::rl::math::Real>::result_type
+        ::std::uniform_real_distribution<::rl::math::Real>::result_type
         YourSampler::rand()
         {
             return this->randDistribution(this->randEngine);
         }
 
         void
-        YourSampler::seed(const ::std::mt19937::result_type& value)
+        YourSampler::seed(const ::std::mt19937::result_type &value)
         {
             this->randEngine.seed(value);
         }
+
+        void YourSampler::setGoalBias(rl::math::Vector *goal_a, rl::math::Vector *goal_b, rl::math::Real prob)
+        {
+            this->goal_a = goal_a;
+            this->goal_b = goal_b;
+            this->goal_prob = prob;
+        }
+
+        rl::math::Vector *
+        YourSampler::getGoal(int index) { if (index == 0) return this->goal_a;
+        return this->goal_b;}
+
+
+
     }
 }
-
